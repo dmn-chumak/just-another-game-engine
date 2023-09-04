58f9d041585fb1149aa0c4a863ce48eb8c936c82
Dmitry <5050252+dmn-chumak@users.noreply.github.com>
2023-09-03 21:38:33 -0400

    Added simple SDF text example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { RepeatableBitmap } from './engine/renderer/common/RepeatableBitmap';
+import { TextField } from './engine/renderer/text/TextField';
 import { System } from './engine/system/System';
 
-window.onload = () => {
+window.onload = async () => {
     System.initialize('#wrapper');
+
+    const fontDescription = await (await fetch('./font/TornadoC.font')).arrayBuffer();
+    const fontTexture = System.render.createTexture();
+    fontTexture.uploadFromImage(document.querySelector<HTMLImageElement>('#texture-font'));
+    System.resourceManager.registerBitmapFont('TornadoC', fontTexture, fontDescription);
 
     const texture = System.render.createTexture();
     texture.uploadFromImage(document.querySelector<HTMLImageElement>('#texture'));
@@ -11,6 +17,10 @@ window.onload = () => {
     bitmap.width = window.innerWidth;
     bitmap.height = window.innerHeight;
     System.stage.appendChild(bitmap);
+
+    const field = new TextField('Example', 'TornadoC', 0xFFFFFF, 15);
+    field.x = field.y = 200;
+    System.stage.appendChild(field);
 
     window.onresize = () => {
         bitmap.width = window.innerWidth;
